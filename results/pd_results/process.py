825b7efa95e5626d65850a1ff8eb3658fdfb9289
Rick Ono <rrono@mit.edu>
2023-12-12 17:00:44 -0500

    update results format

--- results/pd_results/process.py
+++ results/pd_results/process.py
@@ -1,8 +1,9 @@
 import json
 
-files = ['sf1','sf2','sf5']
+files = ['sf1', 'sf2', 'sf5', 'sf10']
 for file in files:
     results_dict = {}
+    results_dict['queries'] = {}
     with open(f'{file}.txt', 'r') as results:
         for line in results:
             if 'loading' in line:
@@ -16,12 +17,10 @@ for file in files:
                 continue
             else:
                 colon = line.index(':')
-                print(line)
                 close_b = line.index(']')
                 query = line[:colon]
                 q_result = line[colon+2:close_b+1]
-                results_dict[query] = json.loads(q_result)
+                results_dict['queries'][query] = json.loads(q_result)
     print(results_dict)
     with open(f'{file}.json', 'w') as json_res:
         json.dump(results_dict, json_res)
-
--- results/pg_results/process.py
+++ results/pg_results/process.py
@@ -2,14 +2,16 @@ import json
 files = ['sf1', 'sf2', 'sf5', 'sf10']
 
 for file in files:
-    results_dict = {}
+    query_results = {}
     with open(f'{file}.txt', 'r') as results:
         for line in results:
             colon = line.index(':')
             close_b = line.index(']')
             query = line[:colon]
             results = line[colon+2:close_b+1]
-            results_dict[query] = json.loads(results)
-    print(results_dict)
+            query_num = int(query[1:])
+            query_results[f'Q{query_num:02}'] = json.loads(results)
+    results_dict = {}
+    results_dict['queries'] = query_results
     with open(f'{file}.json', 'w') as json_res:
         json.dump(results_dict, json_res)
